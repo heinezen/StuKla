b2a4f11cf291811875bbfe42a6f97245beb62b9f
Christoph Heine <heinezen@hotmail.de>
2014-12-23 21:03:15 +0100

    Activity erzeugt.

---
+++ app/src/main/java/heinezen/stukla/werkzeuge/TestErgebnisWerkzeug.java
@@ -0,0 +1,45 @@
+package heinezen.stukla.werkzeuge;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import heinezen.stukla.R;
+
+public class TestErgebnisWerkzeug extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_ergebnis);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_test_ergebnis, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if(id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
