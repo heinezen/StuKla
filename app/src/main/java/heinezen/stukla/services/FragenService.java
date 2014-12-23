60ffdb2ca2a8aae505faf82ab40b543afb1cb796
Christoph Heine <heinezen@hotmail.de>
2014-12-23 16:32:43 +0100

    FragenService angepasst an die Benutzung von Fragments.

--- app/src/main/java/heinezen/stukla/services/FragenService.java
+++ app/src/main/java/heinezen/stukla/services/FragenService.java
@@ -9,56 +9,42 @@ import heinezen.stukla.materialien.Frage;
  * @author Christophad
  *
  */
-public interface FragenService 
-{	
-	/**
-	 * Berechnet die Gesamtpunktzahl f�r alle Fragen.
-	 * 
-	 * @return Erreichte Gesamtpunktzahl.
-	 */
-	public int berechneGesamtpunktzahl();
+public interface FragenService
+{
+    /**
+     * Berechnet die Gesamtpunktzahl f�r alle Fragen.
+     *
+     * @return Erreichte Gesamtpunktzahl.
+     */
+    public int berechneGesamtpunktzahl();
 
-	/**
-	 * L�sst den Klienten zur n�chsten Frage springen. Falls die letzte Frage
-	 * schon erreicht ist, wird die erste Frage zur aktuellen Frage.
-	 * 
-	 */
-	public Frage zurNaechstenFrage();
+    /**
+     * Lasst den Klienten zu einer bestimmten Frage springen.
+     *
+     * @param index Index der angew�hlten Frage.
+     *
+     * @require index < _fragensatz.length
+     * @require index >= 0
+     */
+    public Frage getFrage(int index);
 
-	/**
-	 * L�sst den Klienten zur vorherigen Frage springen. Falls die erste Frage
-	 * schon erreicht ist, wird die letzte Frage zur aktuellen Frage.
-	 * 
-	 */
-	public Frage zurVorherigenFrage();
-	
-	/**
-	 * Lasst den Klienten zu einer bestimmten Frage springen.
-	 * 
-	 * @param index Index der angew�hlten Frage.
-	 * 
-	 * @require index < _fragensatz.length
-	 * @require index >= 0
-	 */
-	public Frage zuFrage(int index);
-	
-	/**
-	 * Bringt die Fragen aus dem Fragensatz in eine zuf�llige
-	 * Reihenfolge.
-	 */
-	public Frage[] mischeFragensatz(Frage[] fragensatz);
-	
-	/**
-	 * Gibt die maximale Punktzahl f�r alle Fragen zur�ck.
-	 * 
-	 * @return Maximal erreichbare Punktzahl aller Fragen.
-	 */
-	public int getMaxPunktzahl();
-	
-	/**
-	 * Gibt die aktuelle Frage zur�ck.
-	 * 
-	 * @return Die aktuelle Frage im Fragenservice
-	 */
-	public Frage getAktuelleFrage();
+    /**
+     * Bringt die Fragen aus dem Fragensatz in eine zuf�llige
+     * Reihenfolge.
+     */
+    public Frage[] mischeFragensatz(Frage[] fragensatz);
+
+    /**
+     * Gibt die maximale Punktzahl f�r alle Fragen zur�ck.
+     *
+     * @return Maximal erreichbare Punktzahl aller Fragen.
+     */
+    public int getMaxPunktzahl();
+
+    /**
+     * Gibt die Anzahl der enthaltenen Fragen zurück.
+     *
+     * @return Anzahl der Fragen im Fragenservice
+     */
+    public int getAnzahl();
 }
--- app/src/main/java/heinezen/stukla/services/FragenServiceImpl.java
+++ app/src/main/java/heinezen/stukla/services/FragenServiceImpl.java
@@ -10,115 +10,85 @@ import heinezen.stukla.materialien.Frage;
  */
 public class FragenServiceImpl implements FragenService
 {
-	/**
-	 * Der Fragensatz des Services.
-	 */
-	private final Frage[] _fragensatz;
-	
-	private int _aktuellerIndex;
-	
-	public FragenServiceImpl(Frage[] fragensatz)
-	{
-		_fragensatz = mischeFragensatz(fragensatz);
-		_aktuellerIndex = 0;
-	}
-	
-	public int berechneGesamtpunktzahl()
-	{
-		int gesamtpunktzahl = 0;
-		
-		for(int i = 0; i < _fragensatz.length; ++i)
-		{
-			gesamtpunktzahl += berechnePunktzahlFuerFrage(i);
-		}
-		
-		return gesamtpunktzahl;
-	}
-	
-	/**
-	 * Berechnet die Punktzahl des Spielers f�r eine der Fragen 
-	 * im gespeicherten Fragensatz.
-	 * 
-	 * @param index Index der Frage, f�r die die Punktzahl berechnet werden soll.
-	 * @return Punktzahl f�r die Frage.
-	 */
-	private int berechnePunktzahlFuerFrage(int index)
-	{
-		return _fragensatz[index].vergleicheAntworten();
-	}
+    /**
+     * Der Fragensatz des Services.
+     */
+    private final Frage[] _fragensatz;
 
-	@Override
-	public Frage zurNaechstenFrage() 
-	{
-		if((_aktuellerIndex + 1) < _fragensatz.length)
-		{
-			++_aktuellerIndex;
-		}
-		else
-		{
-			_aktuellerIndex = 0;
-		}
-		
-		return getAktuelleFrage();
-	}
+    public FragenServiceImpl(Frage[] fragensatz)
+    {
+        _fragensatz = mischeFragensatz(fragensatz);
+    }
 
-	@Override
-	public Frage zurVorherigenFrage() 
-	{
-		if((_aktuellerIndex - 1) >= 0)
-		{
-			--_aktuellerIndex;
-		}
-		else
-		{
-			_aktuellerIndex = _fragensatz.length - 1;
-		}
-		
-		return getAktuelleFrage();
-	}
+    /**
+     * Berechnet die Gesamtpunktzahl des Beantworters für alle Fragen.
+     *
+     * @return Endpunktzahl des Tests.
+     */
+    public int berechneGesamtpunktzahl()
+    {
+        int gesamtpunktzahl = 0;
 
-	@Override
-	public Frage zuFrage(int index)
-	{
-		assert index < _fragensatz.length : "Vorbedingung verletzt : index < _fragensatz.length";
-		assert index >= 0 : "Vorbedingung verletzt : index >= 0";
-		
-		_aktuellerIndex = index;
-		
-		return getAktuelleFrage();
-	}
+        for(int i = 0; i < _fragensatz.length; ++i)
+        {
+            gesamtpunktzahl += berechnePunktzahlFuerFrage(i);
+        }
 
-	@Override
+        return gesamtpunktzahl;
+    }
+
+    /**
+     * Berechnet die Punktzahl des Spielers f�r eine der Fragen
+     * im gespeicherten Fragensatz.
+     *
+     * @param index Index der Frage, f�r die die Punktzahl berechnet werden soll.
+     * @return Punktzahl f�r die Frage.
+     */
+    private int berechnePunktzahlFuerFrage(int index)
+    {
+        return _fragensatz[index].vergleicheAntworten();
+    }
+
+    @Override
+    public Frage getFrage(int index)
+    {
+        assert index < _fragensatz.length : "Vorbedingung verletzt : index < _fragensatz.length";
+        assert index >= 0 : "Vorbedingung verletzt : index >= 0";
+
+        return _fragensatz[index];
+    }
+
+    @Override
     public Frage[] mischeFragensatz(Frage[] fragensatz)
     {
-		Frage[] tempFragensatz = fragensatz.clone();
-	    
-	    for(int i = 0; i < tempFragensatz.length; ++i)
-	    {
-	    	int random = (int) (Math.random() * (tempFragensatz.length - 1));
-	    	Frage tempFrage = tempFragensatz[i];
-	    	tempFragensatz[i] = tempFragensatz[random];
-	    	tempFragensatz[random] = tempFrage;
-	    }
-	    
-	    return tempFragensatz;
+        Frage[] tempFragensatz = fragensatz.clone();
+
+        for(int i = 0; i < tempFragensatz.length; ++i)
+        {
+            int random = (int) (Math.random() * (tempFragensatz.length - 1));
+            Frage tempFrage = tempFragensatz[i];
+            tempFragensatz[i] = tempFragensatz[random];
+            tempFragensatz[random] = tempFrage;
+        }
+
+        return tempFragensatz;
     }
 
-	@Override
+    @Override
     public int getMaxPunktzahl()
     {
-	    int maxPunktzahl = 0;
-	    
-	    for(int i = 0; i < _fragensatz.length; ++i)
-	    {
-	    	maxPunktzahl += _fragensatz[i].getMaxPunktzahl();
-	    }
-	    return maxPunktzahl;
+        int maxPunktzahl = 0;
+
+        for(Frage frage : _fragensatz)
+        {
+            maxPunktzahl += frage.getMaxPunktzahl();
+        }
+        return maxPunktzahl;
     }
 
-	@Override
-    public Frage getAktuelleFrage()
+    @Override
+    public int getAnzahl()
     {
-	    return _fragensatz[_aktuellerIndex];
+        return _fragensatz.length;
     }
 }
