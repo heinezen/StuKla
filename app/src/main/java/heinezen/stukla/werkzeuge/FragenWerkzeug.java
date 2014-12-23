4294312e3358d369f67d92be8031a9a70817e5aa
Christoph Heine <heinezen@hotmail.de>
2014-12-23 16:40:30 +0100

    FragenWerkzeug verwaltet nur noch den FragenService. Fragen werden statt über eigene UI in einem ViewPager dargestellt.

--- app/src/main/java/heinezen/stukla/werkzeuge/FragenWerkzeug.java
+++ app/src/main/java/heinezen/stukla/werkzeuge/FragenWerkzeug.java
@@ -2,13 +2,15 @@ package heinezen.stukla.werkzeuge;
 
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.view.View;
-import android.widget.LinearLayout;
+import android.view.Menu;
 
 import heinezen.stukla.R;
 import heinezen.stukla.materialien.Frage;
-import heinezen.stukla.materialien.OptionalFrage;
 import heinezen.stukla.services.FragenService;
 import heinezen.stukla.services.FragenServiceImpl;
 
@@ -20,25 +22,22 @@ import heinezen.stukla.services.FragenServiceImpl;
  */
 public class FragenWerkzeug extends ActionBarActivity
 {
+    private final String ARG_FRAGEN = "Fragen";
+
     /**
      * Der FragenService.
      */
     private FragenService _fragenService;
 
     /**
-     * Die aktuelle Frage.
-     */
-    private Frage _aktuelleFrage;
-
-    /**
-     * Das UI des FragenWerkzeugs.
+     * Die UI des FragenWerkzeugs.
      */
     private FragenWerkzeugUI _uiFragenWerkzeug;
 
     /**
-     * Das AntwortenWerkzeug.
+     * Der Pager mit UI zum Wehseln der Fragen.
      */
-    private AntwortenWerkzeug _antwortenWerkzeug;
+    private ViewPager _fragenPager;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -46,142 +45,41 @@ public class FragenWerkzeug extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragen_beantworter);
 
-        Parcelable[] parc = getIntent().getParcelableArrayExtra("Fragen");
+        Parcelable[] parc = getIntent().getParcelableArrayExtra(ARG_FRAGEN);
         Frage[] fragen = new Frage[parc.length];
         System.arraycopy(parc, 0, fragen, 0, parc.length);
 
-        _fragenService = new FragenServiceImpl(fragen);
+        _fragenService = new FragenServiceImpl(fragen, false);
 
-        _aktuelleFrage = _fragenService.getAktuelleFrage();
+        _fragenPager = (ViewPager) findViewById(R.id._pager);
+        _fragenPager.setAdapter(new FragenPagerAdapter(getSupportFragmentManager()));
 
-        _antwortenWerkzeug = new AntwortenWerkzeug(
-                (LinearLayout) findViewById(R.id._antwortenBereich));
+        _uiFragenWerkzeug = new FragenWerkzeugUI(
+                (android.widget.LinearLayout) findViewById(R.id._punkteAnzeige));
 
-        _uiFragenWerkzeug = new FragenWerkzeugUI((LinearLayout) findViewById(R.id._anzeige),
-                _antwortenWerkzeug.getPanel());
-
-        _antwortenWerkzeug.setAktuellenFragetyp(_aktuelleFrage.getFragetyp());
-
-        registriereButtons();
-
-        aendereElemente();
+        registrierePager();
     }
 
-    private void registriereButtons()
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
     {
-        _uiFragenWerkzeug.getAbgabeButton().setOnClickListener(new View.OnClickListener()
+        getMenuInflater().inflate(R.menu.menu_fragen_beantworter, menu);
+        return true;
+    }
+
+    /**
+     * Registriert die Listener des Pagers.
+     */
+    private void registrierePager()
+    {
+        _fragenPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
         {
             @Override
-            public void onClick(View v)
+            public void onPageSelected(int pagePosition)
             {
-                testAbgeben();
+                invalidateOptionsMenu();
             }
         });
-        _uiFragenWerkzeug.getVorwaertsButton().setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                naechsteFrage();
-            }
-        });
-        _uiFragenWerkzeug.getRueckwaertsButton().setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                vorherigeFrage();
-            }
-        });
-    }
-
-    /**
-     * �ndert die GUI-Elemente und passt sie an die Frage an.
-     */
-    private void aendereElemente()
-    {
-        aendereFrage();
-        aendereQuelltext();
-        aendereBild();
-        aendereAntworten();
-    }
-
-    private void aendereBild()
-    {
-        if(_aktuelleFrage instanceof OptionalFrage)
-        {
-            if(((OptionalFrage) _aktuelleFrage).hatBild())
-            {
-                _uiFragenWerkzeug.aktualisiereBild(((OptionalFrage) _aktuelleFrage).getBild());
-            }
-            else
-            {
-                _uiFragenWerkzeug.entferneBild();
-            }
-        }
-        else
-        {
-            _uiFragenWerkzeug.entferneBild();
-        }
-    }
-
-    /**
-     * �ndert den angezeigten Fragebereich.
-     */
-    private void aendereFrage()
-    {
-        _uiFragenWerkzeug.aktualisiereFrage(_aktuelleFrage.getFragetext());
-    }
-
-    /**
-     * �ndert den angezeigten Quelltext
-     */
-    private void aendereQuelltext()
-    {
-        if(_aktuelleFrage instanceof OptionalFrage)
-        {
-            _uiFragenWerkzeug.aktualisiereQuelltext(
-                    ((OptionalFrage) _aktuelleFrage).getQuelltext());
-        }
-        else
-        {
-            _uiFragenWerkzeug.aktualisiereQuelltext("");
-        }
-    }
-
-    /**
-     * �ndert den angezeigten Antwortenbereich.
-     */
-    private void aendereAntworten()
-    {
-        _antwortenWerkzeug.aktualisiereAntworten(_aktuelleFrage.getAntworttexte(),
-                _aktuelleFrage.getSpielerAntworten());
-    }
-
-    /**
-     * Springt zur n�chsten Frage im aktuellen Test.
-     */
-    private void naechsteFrage()
-    {
-        _aktuelleFrage.aktualisiereSpielerAntworten(_antwortenWerkzeug.getEingaben());
-
-        _aktuelleFrage = _fragenService.zurNaechstenFrage();
-        _antwortenWerkzeug.setAktuellenFragetyp(_aktuelleFrage.getFragetyp());
-
-        aendereElemente();
-    }
-
-    /**
-     * Springt zur vorherigen Frage im aktuellen Test.
-     */
-    private void vorherigeFrage()
-    {
-        _aktuelleFrage.aktualisiereSpielerAntworten(_antwortenWerkzeug.getEingaben());
-
-        _aktuelleFrage = _fragenService.zurVorherigenFrage();
-        _antwortenWerkzeug.setAktuellenFragetyp(_aktuelleFrage.getFragetyp());
-
-        aendereElemente();
     }
 
     /**
@@ -190,12 +88,32 @@ public class FragenWerkzeug extends ActionBarActivity
      */
     private void testAbgeben()
     {
-        _aktuelleFrage.aktualisiereSpielerAntworten(_antwortenWerkzeug.getEingaben());
-
         int endergebnis = _fragenService.berechneGesamtpunktzahl();
         int maxErgebnis = _fragenService.getMaxPunktzahl();
 
-        _antwortenWerkzeug.setzeBeendet();
         _uiFragenWerkzeug.beendeTest(endergebnis, maxErgebnis);
+    }
+
+    /**
+     * Implementiert einen PagerAdapter der FragenFragments erzeugt.
+     */
+    private class FragenPagerAdapter extends FragmentStatePagerAdapter
+    {
+        public FragenPagerAdapter(FragmentManager fragmentManager)
+        {
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int fragenIndex)
+        {
+            return FragenFragment.newInstance(_fragenService.getFrage(fragenIndex));
+        }
+
+        @Override
+        public int getCount()
+        {
+            return _fragenService.getAnzahl();
+        }
     }
 }
